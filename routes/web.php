5aac3a51cccd92fda03b6e4d0a0b8c3b74737c37
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-20 11:53:51 +0900

    add job

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Jobs\ProcessRefund;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/refund', function () {
+    ProcessRefund::dispatch();
+
+    return 'Refund processed';
 });
